8a1846d8bc83f0457f55baf6fb67519eeb688e49
ソンカイヨウ <k021g1268@g.neec.ac.jp>
2022-05-24 15:58:57 +0900

    Mat4調整

--- Transform.cpp
+++ Transform.cpp
@@ -5,11 +5,6 @@ Transform::Transform() :
 {
 }
 
-//Transform::Transform(XMFLOAT3 pos, XMFLOAT3 scale, XMFLOAT3 rot) :
-//	pos(pos), scale(scale), rot(rot)
-//{
-//}
-
 Transform::Transform(Vec3 pos, Vec3 scale, Vec3 rot) :
 	pos(pos), scale(scale), rot(rot)
 {
@@ -19,15 +14,15 @@ void Transform::Update()
 {
 	// スケーリング
 	matScale = Mat4::Scale(scale);
-	matWorld = matWorld * matScale;
+	matWorld *= matScale;
 
 	// 回転
-	matRot = matRot * Mat4::RotateZ(rot.z);
-	matRot = matRot * Mat4::RotateX(rot.x);
-	matRot = matRot * Mat4::RotateY(rot.y);
-	matWorld = matWorld * matRot;
+	matRot *= Mat4::RotateZ(rot.z);
+	matRot *= Mat4::RotateX(rot.x);
+	matRot *= Mat4::RotateY(rot.y);
+	matWorld *= matRot;
 
 	// 平行移動
 	matTrans = Mat4::Translate(pos);
-	matWorld = matWorld * matTrans;	// ワールド行列に平行移動を反映
+	matWorld *= matTrans;	// ワールド行列に平行移動を反映
 }
