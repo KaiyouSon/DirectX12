d97e64e60260736c298df8245718278fb2dd160c
ソンカイヨウ <k021g1268@g.neec.ac.jp>
2022-07-11 17:33:31 +0900

    デバッグでリリース時のウィンドウサイズを変更可能にした

--- NewEngine/Header/Render/Viewport.h
+++ NewEngine/Header/Render/Viewport.h
@@ -1,7 +1,25 @@
 #pragma once
+#include "NewEngine/Header/Developer/Math/Vec2.h"
+#include <memory>
+
 class Viewport
 {
+private:
+	Vec2 leftTopPos;
+	Vec2 size;
+	float MinDepth;
+	float MaxDepth;
+
 public:
 	void Update();
+
+	void SetViewport(const Vec2& leftTopPos, const Vec2& size, float MinDepth = 0.0f, float MaxDepth = 1.0f);
+
+	Vec2 GetLeftTopPos();
+	Vec2 GetSize();
+	float GetMinDepth();
+	float GetMaxDepth();
 };
+
+extern std::unique_ptr<Viewport> viewport;
 
